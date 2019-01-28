21abbb2389bad8698a4970181b3c64160b40b3bd
longnguyendr <long.nguyendr@gmail.com>
2019-01-28 03:19:25 +0200

    inital submit

---
+++ src/components/Search_bar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Search_bar extends Component {
+    constructor(props) {
+        super(props);
+      }
+    render () {
+        return(
+            <div>
+            {/* <p>{this.props.task.join(" , ")}</p> */}
+                <form  onSubmit={this.props.getWeather}>
+                    <input type="text" placeholder="Enter city" name="city" value={this.props.value} 
+                    onChange={this.props.handleChange}></input>
+                    <button >Add city</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search_bar
