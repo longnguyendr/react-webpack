d724fb178a37f5bcd84dce628a8c9ac160166b23
long.nguyendr <long.nguyendr@gmail.com>
2019-03-04 21:51:01 +0200

    update stage 1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Title from './components/Title';
 import Search_bar from './components/Search_bar';
 import Weather from './components/Weather';
 import styles from './components/Suggestion.css';
+import axios from 'axios';
 
+const apiKey = require('../apiKey.json');
 const cityData = require('../www/citylist/citylist.json');
-const API_KEY = "d0ae4271fe238991dc50293837d2f193";
 
 class App extends Component {
     constructor(props) {
@@ -15,6 +16,7 @@ class App extends Component {
             data: [],
             city_name: '',
             cityInput: '',
+            cityId: '',
             submit: '',
             errormessage: '',
             country: '',
@@ -22,6 +24,7 @@ class App extends Component {
         };
         this.handleChange = this.handleChange.bind(this);
         this.getWeather = this.getWeather.bind(this);
+        this.handleClick = this.handleClick.bind(this);
       }
     // set delay times
     delayTime = async (ms) => {
@@ -34,36 +37,63 @@ class App extends Component {
         this.setState({
           cityInput: e.target.value
         });
-        if(e.target.value != null){
+        
+        if(this.state.cityInput != null){
             this.setState({dropdown: 'dropdown-open'});
-            await this.delayTime(500);
+            await this.delayTime(800);
             let cityOutput;
-            cityOutput = cityData.filter(i => i.name.trim().toLowerCase().startsWith(this.state.cityInput.toLowerCase().trim())); // change code here
-            cityOutput = (cityOutput.length === 0) ? [] : cityOutput.slice(0,5);
+            let cityInputLength = this.state.cityInput.length;
+            let cityInput = this.state.cityInput.toLowerCase().trim();
+            // cityOutput = cityData.filter(i => i.name.trim().toLowerCase().startsWith(this.state.cityInput.toLowerCase().trim())); // change code here
+            
+            cityOutput = cityData.filter(i => {
+                if(i.name.trim().toLowerCase().startsWith(cityInput)) {
+                    if(i.name.length === cityInputLength)
+                    {
+                        return i.name.trim().toLowerCase() === (cityInput); 
+                    }
+                    else if (i.name.length > cityInputLength) {
+                        return i.name.trim().toLowerCase().slice(0, cityInputLength) === (cityInput); 
+                    }
+                }
+            }); 
+            cityOutput = (cityOutput.length === 0) ? [] : cityOutput.slice(0,10);
             let results = (this.state.cityInput.length > 0) ? this.setState({data: cityOutput}) : this.setState({data:[]});
+            // console.log(this.state.data);
             return results
         }
     }
     // Suggestion on Click event
     handleClick = (i) => {
-        console.log(i.target.id);
+        // console.log();
         this.setState({
             dropdown: "dropdown-close",
-            cityInput: i.target.id
+            cityInput: i.target.getAttribute('name'),
+            cityId: i.target.id
         });
     }
 
     // get weather on submit button
     getWeather = async (e) => {
         e.preventDefault();
-        this.setState({
-            submit: e.target.elements.city.value
-        });
-        const citys = e.target.elements.city.value;
+        return new Promise((resolve, reject) => {
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?id=${this.state.cityId}&APPID=${apiKey.openWeatherKey}&units=metric`)
+                 .then( (response) => {
+                     // handle success
+                     console.log(response);
+                     console.log(response.data);
+                     resolve(response.data);
+                 })
+                 .catch( (error) => {
+                     // handle error
+                     console.log(error);
+                     reject(error);
+                 });     
+         });
     }
     render (){
         // console.log(this.state.data);
-        const data = this.state.data.map((i) => <li className={this.state.dropdown} onClick={this.handleClick} id={i.name}> {i.name}</li>)
+        const data = this.state.data.map((i) => <li className={this.state.dropdown} onClick={this.handleClick} id={i.id} name={i.name}> {i.name}, {i.country}</li>)
         return(
             <div>
                 <Title />
@@ -74,13 +104,13 @@ class App extends Component {
                 />
                 <Weather />
                 <div>
-                    <ul > 
+                    <ul> 
                         {data} 
                     </ul>
                 </div>
-                <ul>
+                {/* <ul>
                     <li>cityInput = {this.state.cityInput}</li>
-                </ul>
+                </ul> */}
             </div>            
         );
     }
